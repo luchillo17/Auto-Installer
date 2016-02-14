31fcf94f4a0e1689c5a9c3605c102c42f199b218
Luchillo <luchillo17@gmail.com>
2016-02-14 09:54:22 -0500

    Better curl progress bar

--- start.sh
+++ start.sh
@@ -15,6 +15,7 @@ fi
 
 if ! [[rails -v &> /dev/null =~ 'rails']]; then
   echo 'Installing Rails'
+  echo progress-bar > ~/.curlrc
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   \curl -sSL https://get.rvm.io | bash -s stable --ruby --rails
 fi
