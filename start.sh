d1ae1dd347b981129d071ef515a1384e8176c22e
Luchillo <luchillo17@gmail.com>
2016-02-13 22:10:22 -0500

    Fix wrong source path for rvm

--- start.sh
+++ start.sh
@@ -10,5 +10,5 @@ if ! [[rails -v &> /dev/null =~ 'rails']]; then
   echo 'Installing Rails'
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   \curl -sSL https://get.rvm.io | bash -s stable --rails
-  source ~/.rvm/scripts/rvm
+  source /usr/local/rvm/scripts/rvm
 fi
