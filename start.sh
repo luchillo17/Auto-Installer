b5bfbc0e9856e035cd8f12ebe2ae76505bb3b36a
Luchillo <luchillo17@gmail.com>
2016-02-13 21:48:25 -0500

    Fix space breaking if conditional

--- start.sh
+++ start.sh
@@ -6,7 +6,8 @@ set -o errexit
 
 # Here's inverted commilla just in case `
 
-if ! [[ rails -v &> /dev/null =~ 'rails' ]]; then
+if ! [[rails -v &> /dev/null =~ 'rails']]; then
+  echo 'Installing Rails'
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   \curl -sSL https://get.rvm.io | bash -s stable --rails
   source ~/.rvm/scripts/rvm
